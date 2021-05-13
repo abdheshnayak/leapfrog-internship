f1ecdb1ef81c33ef2260835c66e7ff8b075c274b
abdheshnayak <abdheshnayak@gmail.com>
2021-05-13 10:59:16 +0545

    js ass4 update

---
+++ Assignment/Assignment4-simultanius/js/index.js
@@ -0,0 +1,10 @@
+var gameObj = new Game();
+// this.gameObj.startAnimating(50);
+
+document.getElementById("play").addEventListener(
+  "click",
+  (e) => {
+    this.gameObj.startAnimating(50);
+  },
+  this
+);
